884465e6b01eda72fbf298ecc5916ac2ff2fd51d
oKiraAlter <138947756+oKiraAlter@users.noreply.github.com>
2023-11-08 12:03:06 -0300

    Update ambiente.js

---
+++ cursojs/aula13/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1
+do {
+    console.log(`Download ${c}% concluído.`)
+    c++
+} while ( c <= 100)
+
+
+
+
+/*var c = 1
+while ( c <= 100) {
+    console.log(`Download ${c}% concluído.`)
+    c++
+}*/
