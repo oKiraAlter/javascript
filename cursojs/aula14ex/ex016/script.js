c21d566f525654ea1029a064fe769140d6587c50
oKiraAlter <138947756+oKiraAlter@users.noreply.github.com>
2023-11-11 21:54:28 -0300

    fix

--- cursojs/aula14ex/ex016/script.js
+++ cursojs/aula14ex/ex016/script.js
@@ -4,9 +4,31 @@ function contar() {
     var passo = document.getElementById('passo')
     var res = document.getElementById('res')
 
-    while (Number.inicio <= Number.fim) {
-        inicio =+ passo
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        //window.alert('[ERRO] Faltam Dados!')
+        res.innerHTML = 'Impossível Contar!'
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo Inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+        //Contagem crescente
+         for (let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} \u{1F449}`
+        }
 
-        res.innerHTML += `Contando: ${inicio}`
-    } 
+        } else {
+        //Contagem regressiva
+            for (c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}` 
+        
+    }
 }
