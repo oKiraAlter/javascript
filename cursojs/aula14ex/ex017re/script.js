0268f92b2b135cbf8f77479312a90e58bdd577f8
oKiraAlter <138947756+oKiraAlter@users.noreply.github.com>
2023-11-17 13:18:16 -0300

    remake

--- cursojs/aula14ex/ex017re/script.js
+++ cursojs/aula14ex/ex017re/script.js
@@ -1,16 +1,16 @@
 function gerar() {
-    var num = document.getElementById('num')
-    var tab = document.getElementById('seltab')
+    let num = document.getElementById('num')
+    let tab = document.getElementById('seltab')
 
     if (num.value.length == 0) {
-        window.alert('Por favor digite um número!')
+        window.alert('[ERRO] Digite um número válido!')
     } else {
-        var n = Number(num.value)
-        var c = 1
+        let n = Number(num.value)
+        let c = 1
         tab.innerHTML = ''
         while (c <= 10) {
-            var item = document.createElement('option')
-            item.text = `${n} * ${c} = ${n*c}`
+            let item = document.createElement('option')
+            item.innerHTML = `${n} * ${c} = ${n*c}`
             item.value = `tab${c}`
             tab.appendChild(item)
             c++
